ce0bd5f4b229c7e9388da71b1ccc22bc84a3e509
Hector Lozano <hlozano2@cnm.edu>
2016-10-12 11:14:13 -0600

    Added index.php

---
+++ public_html/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+
+		<!-- add CSS file -->
+
+		<title>Navy Toghether We Served</title>
+	</head>
+	<body>
+
+		<!-- this is the header -->
+		<header>
+			<h1></h1>
+		</header>
+
+		<!-- this is the main page content -->
+		<main>
+
+			<!-- begin section 1 -->
+
+			<section>
+				<p>
+
+				</p>
+				<!-- Need to add image here -->
+			</section>
+
+			<!-- begin section 2 -->
+			<section>
+				<h2> My Favorite Things</h2>
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</section>
+
+			<!-- begin section 3-->
+			<section>
+				<table>
+					<tr>
+						<th></th>
+						<th></th>
+					</tr>
+
+					<tr>
+						<td></td>
+						<td></td>
+
+					</tr>
+
+					<tr>
+						<td></td>
+						<td></td>
+
+					</tr>
+
+					<tr>
+						<td></td>
+						<td></td>
+					</tr>
+				</table>
+			</section>
+		</main>
+	</body>
+</html>
