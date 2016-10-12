5603f085718bb97e201e078f477d525447c3704b
Hector Lozano <hlozano2@cnm.edu>
2016-10-12 13:07:05 -0600

    Added index.php

--- public_html/index.php
+++ public_html/index.php
@@ -8,62 +8,21 @@
 		<title>Navy Toghether We Served</title>
 	</head>
 	<body>
+		<p>Name:Devil Bill
+			Age: 50
+			Profession: Bill is a hard core former Operation Iraqi Freedom Veteran, veteran looking to stay in touch with all current or former shipmates based on common duty stations.
+		</p>
 
-		<!-- this is the header -->
-		<header>
-			<h1></h1>
-		</header>
+		<p>
+		Technology:
+		</p>
 
-		<!-- this is the main page content -->
-		<main>
+		<p>
+		Attitude and Behaviors:
+		</P>
 
-			<!-- begin section 1 -->
-
-			<section>
-				<p>
-					An active duty, reserve member or retired navy veteran looking to stay in touch with all current or
-					former shipmates based on common duty stations.
-				</p>
-				<!-- Need to add image here -->
-			</section>
-
-			<!-- begin section 2 -->
-			<section>
-				<h2> My Favorite Things</h2>
-				<ul>
-					<li></li>
-					<li></li>
-					<li></li>
-					<li></li>
-				</ul>
-			</section>
-
-			<!-- begin section 3-->
-			<section>
-				<table>
-					<tr>
-						<th></th>
-						<th></th>
-					</tr>
-
-					<tr>
-						<td></td>
-						<td></td>
-
-					</tr>
-
-					<tr>
-						<td></td>
-						<td></td>
-
-					</tr>
-
-					<tr>
-						<td></td>
-						<td></td>
-					</tr>
-				</table>
-			</section>
-		</main>
+		</P>
+		Frustrations and Needs:
+		Goals:</p>
 	</body>
 </html>
