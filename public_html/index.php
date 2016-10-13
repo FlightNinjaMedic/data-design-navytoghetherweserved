c7a362fda2ac906a005bcaf5fa740588cd7e90b5
Hector Lozano <hlozano2@cnm.edu>
2016-10-13 13:45:37 -0600

    Added more to html file

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8"/>
 
 		<!-- add CSS file -->
-		<link href="css/stylesheet.css" rel="stylesheet" type="text/css"
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css">
 
 		<title>Navy Toghether We Served</title>
 	</head>
@@ -30,19 +30,27 @@
 				Technology: Extremely technologically challenged, Was a marine who's career time was totally spent in an infantry unit. Served five back to back tours in Iraq and two more in Afghanistan. Owns a Motorola Razor flip phone and has no interest in ever owning a smart phone.
 			</p>
 			<p>
-				Attitude and Behaviors: Now retired, Bill has an outstanding attitude towards life, loves to fish and camping. Once a month he gets toghether with other retired members of the armed forces at Cabezon Wounded Warrior Haven for some real life war games. As Bill puts it: "You can take Bill out of the marines, but you will never take the marine out of Bill"
+				Attitude and Behaviors: Now retired, Bill has an outstanding attitude towards life, loves to fish and camp. Once a month he gets toghether with other retired members of the armed forces at Cabezon Wounded Warrior Haven for some real life war games. As Bill puts it: "You can take Bill out of the marines, but you will never take the marine out of Bill"
 			</p>
 			<p>
-				Frustrations and Needs:
+				Frustrations and Needs:  Like a large percentage of military veterans, Bill suffers from PTSD and other injuries, too many to list.  Bill is a member of the local VFW and even though this has been the primary way he interacts with friends, he is sick and tired of the non-stop beer drinking. Bill wishes there was a better way to stay in touch with friends that served with him.
 			</p>
 			<p>
 				Goals:
 			</p>
 				</fieldset>
 		</section>
-		<section>
-			<h2>Use Case</h2>
 
+		<section>
+			<fieldset>
+			<h2>Use Case</h2>
+				<p>
+					Bill has taken a leap of faith and bought a used Acer laptop.
+				</p>
+				<p>
+					Bill has registered with Navy Toghether We Served and since reconnected with a number of friends. Hard to believe but he has expressed a interest in a smart phone so that he can stay in touch with his friends while on the go.
+				</p>
+			</fieldset>
 		</section>
 	</body>
 </html>
