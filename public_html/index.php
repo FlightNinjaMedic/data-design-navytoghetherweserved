adb9d3bc3c51aca02403168b8400b19b375e0891
Hector Lozano <hlozano2@cnm.edu>
2016-10-12 21:16:04 -0600

    Added css and image

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8"/>
 
 		<!-- add CSS file -->
-		<link href="css/styleseet.css" rel="stylesheet" type="text/css"
+		<link href="css/stylesheet.css" rel="stylesheet" type="text/css"
 
 		<title>Navy Toghether We Served</title>
 	</head>
@@ -14,9 +14,16 @@
 	<body>
 		<section>
 			<h2>Persona</h2>
-			<p>Name:Devil Bill
+			<p>
+				Name: Devil Bill
+			</p>
+			<p>
 			Age: 50
-			Profession: Bill is a hard core former Operation Iraqi Freedom Veteran, veteran looking to stay in touch with all current or former shipmates based on common duty stations.</p>
+			</p>
+
+				<p>
+			     Profession: Bill is a hard core former Operation Iraqi Freedom Veteran, veteran looking to stay in touch with all current or former Devil Doc's based on common duty stations.
+				</p>
 
 			<p>
 				Technology: Extremely technologically challenged, Was a marine who's career time was totally spent in an infantry unit. Served five back to back tours in Iraq and two more in Afghanistan. Owns a Motorola Razor flip phone and has no interest in ever owning a smart phone.
@@ -29,7 +36,7 @@
 			</p>
 			<p>
 				Goals:
-			</p>
+			</h3></p>
 		</section>
 		<section>
 			<h2>Use Case</h2>
